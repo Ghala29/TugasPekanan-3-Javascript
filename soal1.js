b10f8cadf28d91f83cc94411156613477ac6258b
Ghala29 <ghalasudana24@gmail.com>
2023-02-04 15:04:24 +0700

    new file:   soal1.js new file:   soal2.js

---
+++ soal1.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const number = prompt("Masukkan angka: ");
+
+if (!isNaN(number) && number >= 0) {
+ 
+  if (number % 2 === 0) {
+    console.log(`akar pangkat dari ${number} adalah ` + Math.sqrt(number)); 
+  } else {
+    console.log("Tidak bisa input bilangan ganjil"); 
+  }
+} else { // jika inputan salah
+  console.log("Input salah");
+}
